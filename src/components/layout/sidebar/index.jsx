27d389c881638e5167d99b1b6c675c3eaef036a3
Satish Pansara <satish.pansara@agileinfoways.com>
2023-03-01 19:05:47 +0530

    01-03-2023

--- src/components/layout/sidebar/index.jsx
+++ src/components/layout/sidebar/index.jsx
@@ -154,7 +154,7 @@ const SideBar = () => {
         collapsible
         collapsed={collapsed}
         collapsedWidth={0}
-        breakpoint="sm"
+        breakpoint="sm" // Update the breakepoints
       >
         <div id="main-div" className=" flex flex-col h-full bg-white">
           <Menu
